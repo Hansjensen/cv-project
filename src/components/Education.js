6851610547f1c266e0b70d156b487991e70619ac
hansjensen <grenademisfits@gmail.com>
2023-06-30 18:52:53 -0500

    Complete add education function

--- src/components/Education.js
+++ src/components/Education.js
@@ -21,6 +21,8 @@ class Education extends Component {
         this.handleSchoolChange = this.handleSchoolChange.bind(this)
         this.handleRemark1Change = this.handleRemark1Change.bind(this)
         this.handleRemark2Change = this.handleRemark2Change.bind(this)
+        this.handleAddPop = this.handleAddPop.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleRemove = (e) => {
@@ -52,20 +54,52 @@ class Education extends Component {
         })
     }
 
+    handleAddPop = () => {
+        console.log('k')
+        this.setState(prevState => ({
+            add: !prevState.edit
+            }))
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        this.setState({
+            list: this.state.list.concat({
+                degree: this.state.degree,
+                school: this.state.school,
+                remark1: this.state.remark1,
+                remark2: this.state.remark2,
+                id: this.state.id,
+            }),
+            degree: "",
+            school: "",
+            remark1: "",
+            remark2: "",
+            id: uniqid(),
+        })
+        this.setState(prevState => ({
+            add: !prevState.add
+            }))
+    }
+
     render () {
+        
         return (
             <>
-            <PopUpAdd
-                degree={this.handleDegreeChange}
-                school={this.handleSchoolChange}
-                remark1={this.handleRemark1Change}
-                remark2={this.handleRemark2Change}/>
+            {this.state.add &&
+                <PopUpAdd
+                    degree={this.handleDegreeChange}
+                    school={this.handleSchoolChange}
+                    remark1={this.handleRemark1Change}
+                    remark2={this.handleRemark2Change}
+                    addEdu={this.handleSubmit}/>}
             <div id="edu">
                 <EduList
                     list={this.state.list}
                     remove={this.handleRemove}
                 />
-                <button id="addEduButt">Add School</button>
+                <button id="addEduButt" onClick={this.handleAddPop}>Add School</button>
             </div>
             </>
             
@@ -124,7 +158,7 @@ class PopUpAdd extends Component {
                     <label htmlFor="remark2">Remarks</label>
                     <input type="text" id="remark2Input" onChange={this.props.remark2}></input>
          
-                    <button onClick={this.props.addEdu}>Add Work</button>
+                    <button onClick={this.props.addEdu}>Add School</button>
                         
                     
                 </form>
