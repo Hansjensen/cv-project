0276154bd4aafcff45cd339357fe8b8f66d42490
hansjensen <grenademisfits@gmail.com>
2023-06-30 15:32:55 -0500

    Complete Change functions

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import '../styles/Work.css'
-import uniqid from 'uniqid'
+import uniqid from "uniqid"
 
 class Work extends Component {
     constructor () {
@@ -16,7 +16,7 @@ class Work extends Component {
                 job1:"",
                 job2:"",
                 job3:"",
-                id: 21
+                id: uniqid()
             },
             experiences: [{
                 company: "Mcdonalds Inc",
@@ -35,6 +35,15 @@ class Work extends Component {
 
         this.handleRemove = this.handleRemove.bind(this)
         this.handleAddPop = this.handleAddPop.bind(this)
+        this.handleLocationChange = this.handleLocationChange.bind(this)
+        this.handleCompanyChange = this.handleCompanyChange.bind(this)
+        this.handleStartDateChange = this.handleStartDateChange.bind(this)
+        this.handleEndDateChange = this.handleEndDateChange.bind(this)
+        this.handlePositionChange =this.handlePositionChange.bind(this)
+        this.handleJob1Change = this.handleJob1Change.bind(this)
+        this.handleJob2Change = this.handleJob2Change.bind(this)
+        this.handleJob3Change = this.handleJob3Change.bind(this)
+        this.handleAddWorkSubmit =this.handleAddWorkSubmit.bind(this)
     }
 
     handleRemove = (e) => {
@@ -51,13 +60,134 @@ class Work extends Component {
 
     }
 
+    handleCompanyChange = (e) => {
+        this.setState({
+            experience:{
+            company: e.target.value
+            }
+        }
+        )
+        
+    }
+    
+    
+    handleLocationChange = (e) => {
+        this.setState({
+            experience:{
+            location: e.target.value
+            }
+        }
+        )
+        
+    }
+
+    handleStartDateChange = (e) => {
+        this.setState({
+            experience:{
+            startDate: e.target.value
+            }
+        }
+        )
+        
+    }
+    
+    handleEndDateChange = (e) => {
+        this.setState({
+            experience:{
+            endDate: e.target.value
+            }
+        }
+        )
+        
+    }
+
+    handlePositionChange = (e) => {
+        this.setState({
+            experience:{
+            position: e.target.value
+            }
+        }
+        )
+        
+        
+    }
+
+    handleJob1Change = (e) => {
+        this.setState({
+            experience:{
+            job1: e.target.value
+            }
+        }
+        )
+        console.log(this.state.experience.job1)
+        
+    }
+
+    handleJob2Change = (e) => {
+        this.setState({
+            experience:{
+            job2: e.target.value
+            }
+        }
+        )
+        console.log(this.state.experience.job2)
+        
+    }
+
+    handleJob3Change = (e) => {
+        this.setState({
+            experience:{
+            job3: e.target.value
+            }
+        }
+        )
+        console.log(this.state.experience.job3)
+        
+    }
+
+    handleAddWorkSubmit = (e) => {
+        e.preventDefault()
+        this.setState({
+            experiences: this.state.experiences.concat(this.state.experience),
+            experience: {
+                company: "",
+                location:"",
+                startDate: "",
+                endDate: "",
+                position:"",
+                job1:"",
+                job2:"",
+                job3:"",
+                id: uniqid()
+            }
+        })
+        console.log(this.state.experiences)
+        this.setState(prevState => ({
+            add: !prevState.add
+            }))
+        
+    }
+
+    
+
+
     render() {
         const {add} = this.state
         return (
         <div className="workDiv">
             <WorkList handleRemove={this.handleRemove} experiences={this.state.experiences}/>
             {add && 
-                <PopUpAdd />}
+                <PopUpAdd 
+                    company={this.handleCompanyChange}
+                    location={this.handleLocationChange}
+                    startDate={this.handleStartDateChange}
+                    endDate={this.handleEndDateChange}
+                    position={this.handlePositionChange}
+                    job1={this.handleJob1Change}
+                    job2={this.handleJob2Change}
+                    job3={this.handleJob3Change}
+                    addWork={this.handleAddWorkSubmit}
+                    />}
             <button id="addWorkButt" onClick={this.handleAddPop}>Add Work</button>
         </div>
         
@@ -125,22 +255,22 @@ class PopUpAdd extends Component {
             <div className="popUpAdd">
                 <form id="popForm">
                     <label htmlFor="companyInput">Company</label>
-                    <input type="text" id="companyInput"></input>
+                    <input type="text" id="companyInput" onChange={this.props.company}></input>
                     <label htmlFor="locationInput">Location</label>
-                    <input type="text" id="locationInput"></input>
+                    <input type="text" id="locationInput" onChange={this.props.location}></input>
                     <label htmlFor="startDate">Start Date</label>
-                    <input type="text" id="startDate"></input>
+                    <input type="text" id="startDate" onChange={this.props.startDate}></input>
                     <label htmlFor="endDate">End Date</label>
-                    <input type="text" id="endDate"></input>
+                    <input type="text" id="endDate" onChange={this.props.endDate}></input>
                     <label htmlFor="position">Position</label>
-                    <input type="text" id="position"></input>
+                    <input type="text" id="position" onChange={this.props.position}></input>
                     <label htmlFor="job1">Job Duty</label>
-                    <input type="text" id="job1"></input>
+                    <input type="text" id="job1" onChange={this.props.job1}></input>
                     <label htmlFor="job2">Job Duty</label>
-                    <input type="text" id="job2"></input>
+                    <input type="text" id="job2" onChange={this.props.job2}></input>
                     <label htmlFor="job3">Job Duty</label>
-                    <input type="text" id="job3"></input>
-                    <button>Add Work</button>
+                    <input type="text" id="job3" onChange={this.props.job3}></input>
+                    <button onClick={this.handleAddWorkSubmit}>Add Work</button>
                         
                     
                 </form>
