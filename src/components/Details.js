2dff4c202ed545a1ff3b9842b51435138028eb8d
hansjensen <grenademisfits@gmail.com>
2023-07-01 17:39:30 -0500

    Update details.js to functional components.

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,79 +1,34 @@
 import '../styles/details.css'
 import React, {Component, useState} from "react";
 
-function Detailstwo () {
+function Details () {
     const [address, setAddress] = useState("123 Fake St");
     const [address2, setAddress2] = useState ("Noncity, CA 12345")
     const [phone, setPhone] = useState("1-800-321-4321");
     const [email, setEmail] = useState("notanemail@gmail.com")
     const [edit, setEdit] = useState(false)
 
-    
-}
+    const handleToggle = (e) => {
+        setEdit((current) => !current)
+    } 
 
-class Details extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            address: "123 Fake St",
-            address2: "Noncity, CA 12345",
-            phone: "1-800-321-4321",
-            email: "notanemail@gmail.com",
-            edit: false
-        }
-
-        this.handleEdit = this.handleEdit.bind(this)
-        this.handleEmailChange = this.handleEmailChange.bind(this)
-        this.handleAdress2Change = this.handleAdress2Change.bind(this)
-        this.handleAdressChange = this.handleAdressChange.bind(this)
-        this.handlePhoneChange = this.handlePhoneChange.bind(this)
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
     }
 
-    handleEdit (e)  {
-        e.preventDefault()
-        this.setState(prevState => ({
-            edit: !prevState.edit
-            })
-        )
+    const handleAddressChange = (e) => {
+        setAddress(e.target.value)
     }
 
-    handleAdressChange (e) {
-        this.setState({
-            address: e.target.value
-        })
+    const handleAddress2Change = (e) => {
+        setAddress2(e.target.value)
     }
 
-    handleAdress2Change (e) {
-        this.setState({
-            address2: e.target.value
-        })
+    const handlePhoneChange = (e) => {
+        setPhone(e.target.value)
     }
-
-    handlePhoneChange (e) {
-        this.setState({
-            phone: e.target.value
-        })
-    }
-
-    handleEmailChange (e) {
-        this.setState({
-            email: e.target.value
-        })
-    }
-
-    render() {
-
-        const { 
-            address,
-            address2,
-            phone,
-            email,
-            edit
-
-        } = this.state;
-        return (
-        <div className="details">
+    return (
+    <div className="details">
             <DetailTitle title="Address" />
                 <div className="infoBox">
                 { !edit &&
@@ -104,17 +59,13 @@ class Details extends Component {
             <button className="editButt" onClick={this.handleEdit}>Edit</button>
         </div>
         )
-    }
+}
+
+function DetailTitle() {
+    return <h6 className="detailTitle">{this.props.title}</h6>
 }
 
 
-class DetailTitle extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return <h6 className="detailTitle">{this.props.title}</h6>
-    }
-}
+
 
 export default Details;
