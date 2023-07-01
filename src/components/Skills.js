02e5b04f7d2f56cec3142308bbced470d7109444
hansjensen <grenademisfits@gmail.com>
2023-07-01 18:29:07 -0500

    Update Skills to functional Component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,93 +1,57 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import '../styles/skills.css'
 import uniqid from "uniqid"
 
-class Skills extends Component {
-    constructor (props) {
-        super(props)
+function Skills () {
+    const [skill, setSkill] = useState({title: '', id: uniqid()})
+    const [skills, setSkills] = useState([{title: "React", id:23}, {title: "CSS", id:27}, {title: "JavaScript", id:28}, {title: "HTML", id:69}, {title: "Webpack", id:42}, {title: "Git", id:88}])
 
-        this.state = {
-            
-            skill: {
-                title: "",
-                id: uniqid()
-            },
-            skills: [{title: "React", id:23}, {title: "CSS", id:27}, {title: "JavaScript", id:28}, {title: "HTML", id:69}, {title: "Webpack", id:42}, {title: "Git", id:88}]
-        }
-        this.handleAdd = this.handleAdd.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.handleRemove = this.handleRemove.bind(this)
-    }   
-
-    
-
-    handleChange (e) {
-        this.setState({
-            skill: {
-                title: e.target.value,
-                id: this.state.skill.id
-            }
-        })
-    }
-
-    handleAdd () {
-        if(this.state.skill.title === "") {
+    const handleAdd = () => {
+        if(skill.title === "") {
             return;
         }
-        this.setState({
-            skill: {
-                title: "",
-                id: uniqid()
-            },
-            skills: this.state.skills.concat(this.state.skill),
+        setSkills(skills.concat(skill))
+        setSkill({title: '', id: uniqid()})
+    }
+
+    const handleChange = (e) => {
+        setSkill({title: e.target.value})
+
+    }
+
+    const handleRemove = (e) => {
+        setSkills(skills.filter((skill) => skill.title !== e.target.id))
+    }
+
+    return (
+        <div className="skillsList">
+            <SkillsList skills={skills} 
+            handleRemove={handleRemove}/>
             
-        })
-    }
+            
+                <div id="addDiv">
+                  <input type="text" value={skill.title} onChange={handleChange}id="addInput" ></input>
+                  <button id="addSkill"  onClick={handleAdd}>Add</button>
+                </div>
+            
 
-    handleRemove (e) {
-        
-        this.setState({
-            skills: this.state.skills.filter((skill) => skill.title !== e.target.id)
-        })
-    }
-
-    render() {
-
-        const {skills, skill} = this.state
-        return (
-            <div className="skillsList">
-                <SkillsList skills={skills} 
-                handleRemove={this.handleRemove}/>
-                
-                
-                    <div id="addDiv">
-                      <input type="text" value={skill.title} onChange={this.handleChange}id="addInput" ></input>
-                      <button id="addSkill"  onClick={this.handleAdd}>Add</button>
-                    </div>
-                
-
-            </div>
-        )
-    }
+        </div>
+    )
 }
 
-class SkillsList extends Component {
-    constructor (props) {
-        super(props)
-    }
+function SkillsList (props) {
 
-    render () {
-        const {skills} = this.props
-        return <ul className="list">
-            {skills.map((skill) => {
+    return <ul className="list">
+            {props.skills.map((skill) => {
                 return (
                 <div className="skillsDiv">
                     <p> {skill.title}</p>
-                    <button id={skill.title} onClick={this.props.handleRemove}> Remove </button>
+                    <button id={skill.title} onClick={props.handleRemove}> Remove </button>
                 </div>)
             })}
         </ul>
-    }
 }
+
+
 
 export default Skills;
